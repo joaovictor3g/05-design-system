8d06691859ca03b289a94785a3cbee9be8abc5d7
joaovictor3g <jvdias79797@gmail.com>
2022-10-23 11:21:49 -0300

    fix: dependencies fixes

---
+++ packages/docs/src/stories/Button.stories.tsx
@@ -0,0 +1,9 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { Button } from "@joaovictor3g/react";
+
+export default {
+  title: "Button",
+  component: Button,
+} as Meta;
+
+export const Primary: StoryObj = {};
