58d72e94c88eb11a21bf374b1158998c004d8459
joaovictor3g <jvdias79797@gmail.com>
2022-10-23 13:19:14 -0300

    feat: new stories

--- packages/docs/src/stories/Button.stories.tsx
+++ packages/docs/src/stories/Button.stories.tsx
@@ -1,9 +1,21 @@
 import type { Meta, StoryObj } from "@storybook/react";
-import { Button } from "@joaovictor3g/react";
+import { Button, ButtonProps } from "@joaovictor3g/react";
+
+export type StoryObjProps = StoryObj<ButtonProps>;
 
 export default {
   title: "Button",
   component: Button,
-} as Meta;
+} as Meta<ButtonProps>;
 
-export const Primary: StoryObj = {};
+export const Primary: StoryObjProps = {
+  args: {
+    children: "Enviar",
+  },
+};
+
+export const Big: StoryObjProps = {
+  args: {
+    size: "big",
+  },
+};
